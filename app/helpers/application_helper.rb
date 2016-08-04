c32ffe9322c2efa86b292cbff2c3f30f99343d25
kayhide <kayhide@gmail.com>
2016-08-04 22:34:47 +0900

    update views for blank Product#closes_on.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def user_label_for user
     (user == current_user) ? t('nav.my_products') : user
   end
+
+  def display_date time
+    if time
+      if time.future?
+        distance_of_time_in_words_to_now(time) + I18n.t('datetime.after')
+      else
+        distance_of_time_in_words_to_now(time) + I18n.t('datetime.ago')
+      end
+    end
+  end
 end
