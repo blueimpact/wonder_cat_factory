1f008d04cd810104bf53e771381710629871b373
kayhide <kayhide@gmail.com>
2016-08-04 16:27:07 +0900

    update bids view.

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -4,4 +4,8 @@ class Bid < ActiveRecord::Base
 
   validates :user, presence: true
   validates :product, presence: true, uniqueness: { scope: :user }
+
+  def queue_index
+    product.bids.where(Bid.arel_table[:created_at].lt(created_at)).count
+  end
 end
