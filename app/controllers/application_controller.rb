683460c925378a631867dc539e9db151a4b05d2d
kayhide <kayhide@gmail.com>
2016-08-19 16:43:16 +0900

    fix authentication bug.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,13 @@ class ApplicationController < ActionController::Base
   %w(admin seller).each do |role|
     define_method "authenticate_#{role}!" do
       unless current_user && current_user.send("#{role}?")
-        redirect_back_or :root, alert: "This page is for #{role} only."
+        redirect_back_or :root, alert: I18n.t('messages.not_authorized')
         false
       end
     end
+  end
+
+  def redirect_back_or path, *args
+    redirect_to(request.referer || path, *args)
   end
 end
