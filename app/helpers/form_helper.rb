e8ce3822220684dc70037c22abf0e97f3ac12043
kayhide <kayhide@gmail.com>
2016-09-09 01:14:09 +0900

    create FormHelper and InputHelper.

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -4,7 +4,7 @@ module FormHelper
   def initialize *args
     super
     @form_total_cols = 12
-    @form_label_cols = 2
+    @form_label_cols = 3
   end
 
   def form_content_cols
@@ -20,67 +20,63 @@ module FormHelper
     end
   end
 
-  def with_label form, name, options = {}, &proc
-    tag_name = options.delete(:tag_name) || :div
-    options[:class] = Array.wrap(options[:class])
-    options[:class] << content_cols_class
-    content_tag(:div, class: 'form-group') do
-      concat label_tag_of(form, name)
-      concat content_tag(tag_name, capture(&proc), options)
+  def form_group form, name, options = {}, &proc
+    with_feedback form, name, options do
+      with_label form, name, options do
+        capture(&proc)
+      end
     end
+  end
+
+  def form_control options = {}, &proc
+    tag_name = options.delete(:tag_name) || :div
+    content_tag(tag_name, class: options[:class]) do
+      concat capture(&proc)
+      options[:append_proc].try :call
+    end
+  end
+
+  def with_feedback form, name, options = {}, &proc
+    group_class = ['form-group', 'has-feedback']
+    group_class << 'has-error' if form.object.errors.key?(name)
+    options[:append_proc] = proc do
+      concat feedback_icon
+      concat error_messages(form.object.errors.full_messages_for(name))
+    end
+    content_tag(:div, capture(&proc), class: group_class)
+  end
+
+  def without_feedback &proc
+    content_tag(:div, capture(&proc), class: 'form-group')
+  end
+
+  def with_label form, name, options = {}, &proc
+    options[:class] = [options[:class], content_cols_class].flatten
+    concat label_tag_of(form, name)
+    concat form_control(options, &proc)
   end
 
   def without_label options = {}, &proc
-    tag_name = options.delete(:tag_name) || :div
-    options[:class] = Array.wrap(options[:class])
-    options[:class] << content_cols_class(with_offset: true)
-    content_tag(:div, class: 'form-group') do
-      content_tag(tag_name, capture(&proc), options)
+    options[:class] = [options[:class], content_cols_class_with_offset].flatten
+    form_control options, &proc
+  end
+
+  def feedback_icon
+    content_tag(:div, class: 'form-control-feedback') do
+      content_tag(:i, nil, class: 'fa fa-lg')
     end
   end
 
-  def input form, name
-    with_label form, name do
-      form.text_field(name, class: 'form-control')
-    end
-  end
-
-  def input_number form, name
-    with_label form, name do
-      form.number_field(name, class: 'form-control')
-    end
-  end
-
-  def input_password form, name
-    with_label form, name do
-      form.password_field(name, class: 'form-control')
-    end
-  end
-
-  def input_text form, name
-    with_label form, name do
-      form.text_area(name, class: 'form-control', rows: 2)
-    end
-  end
-
-  def input_select form, name, choices
-    with_label form, name do
-      form.select(name, choices, {}, class: 'form-control')
-    end
-  end
-
-  def static object, name, &proc
-    with_label(object, name,
-               tag_name: :p,
-               class: 'form-control-static') do
-      proc ? capture(&proc) : object[name]
+  def error_messages messages
+    content_tag(:div, class: 'help-block') do
+      safe_join messages, tag(:br)
     end
   end
 
   private
 
   def label_tag_of form_or_object, name
-    if form_or_object.respond_to?(:label)
+    if form_or_object.respond_to?(:field_helpers?)
       form_or_object.label(name, class: ['control-label', label_cols_class])
     else
       label_tag(:name, class: ['control-label', label_cols_class]) do
@@ -93,11 +89,11 @@ module FormHelper
     "col-md-#{form_label_cols}"
   end
 
-  def content_cols_class options = {}
-    if options[:with_offset]
-      "col-md-offset-#{form_label_cols} #{content_cols_class}"
-    else
-      "col-md-#{form_content_cols}"
-    end
+  def content_cols_class
+    "col-md-#{form_content_cols}"
+  end
+
+  def content_cols_class_with_offset
+    "col-md-offset-#{form_label_cols} #{content_cols_class}"
   end
 end
---
+++ app/helpers/input_helper.rb
@@ -0,0 +1,49 @@
+module InputHelper
+  include FormHelper
+
+  def input form, name
+    form_group(form, name) do
+      form.text_field(name, class: 'form-control')
+    end
+  end
+
+  def input_number form, name
+    form_group(form, name) do
+      form.number_field(name, class: 'form-control')
+    end
+  end
+
+  def input_password form, name
+    form_group(form, name) do
+      form.password_field(name, class: 'form-control')
+    end
+  end
+
+  def input_text form, name
+    form_group(form, name) do
+      form.text_area(name, class: 'form-control', rows: 2)
+    end
+  end
+
+  def input_select form, name, choices
+    form_group(form, name) do
+      form.select(name, choices, {}, class: 'form-control')
+    end
+  end
+
+  def static object, name, &proc
+    without_feedback do
+      with_label object, name, tag_name: :p, class: 'form-control-static' do
+        proc ? capture(&proc) : object[name]
+      end
+    end
+  end
+
+  def labelless &proc
+    without_feedback do
+      without_label do
+        capture(&proc)
+      end
+    end
+  end
+end
