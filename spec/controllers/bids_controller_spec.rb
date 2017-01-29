83f9be4fa8d11c40b99e79a00dbc9f92bf893245
Masaki Takehara <morisot64@gmail.com>
2017-01-29 12:58:53 +0900

    bid#charge test

--- spec/controllers/bids_controller_spec.rb
+++ spec/controllers/bids_controller_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe BidsController, type: :controller do
   login_user
 
-  let(:product) { FactoryGirl.create(:product, user: @current_user) }
+  let(:seller) { FactoryGirl.create(:user, :seller, :with_stripe_account) }
+  let(:seller_not_have_stripe_account) { FactoryGirl.create(:user, :seller) }
+
+  let(:product) { FactoryGirl.create(:product, user: seller) }
+  let(:product_not_have_stripe_account) {
+    FactoryGirl.create(:product, user: seller_not_have_stripe_account)
+  }
 
   describe 'GET #index' do
     it 'assings bids of current_user as @bids' do
@@ -89,21 +95,49 @@ RSpec.describe BidsController, type: :controller do
   end
 
   describe 'POST #charge' do
-    it 'fails charge if user does not bid' do
-      expect {
-        xhr :post, :charge, { product_id: product.id }
-      }.to raise_error
+    before(:each) do
+      request.env['HTTP_REFERER'] = product_url(product)
     end
-  end
 
-  describe 'POST #charge' do
-    it 'fails charge if user already paid' do
-      FactoryGirl.create(
-        :bid, user: @current_user, product: product, paid_at: Time.current
-      )
-      expect {
-        xhr :post, :charge, { product_id: product.id }
-      }.to raise_error
+    context 'with user does not bid' do
+      it 'redirects to back' do
+        post :charge, { product_id: product.id }
+        expect(response).to redirect_to(product_url(product))
+      end
+    end
+
+    context 'with user already charged' do
+      it 'redirects to back' do
+        FactoryGirl.create(
+          :bid, user: @current_user, product: product, paid_at: Time.current
+        )
+        post :charge, { product_id: product.id }
+        expect(response).to redirect_to(product_url(product))
+      end
+    end
+
+    context 'with user bid and not charged' do
+      it 'seller does not have stripe account' do
+        FactoryGirl.create(
+          :bid, user: @current_user, product: product_not_have_stripe_account
+        )
+        expect {
+          post :charge, { product_id: product_not_have_stripe_account.id }
+        }.to raise_error(
+          RuntimeError, 'Seller account must need stripe account.'
+        )
+      end
+
+      it 'updates paid_at current_time' do
+        time_now = Time.current
+        allow(Time).to receive(:now).and_return(time_now)
+        bid = FactoryGirl.create(
+          :bid, user: @current_user, product: product
+        )
+        post :charge, { product_id: product.id }
+        bid.reload
+        expect(bid.paid_at).to eq time_now
+      end
     end
   end
 end
--- spec/support/stripe.rb
+++ spec/support/stripe.rb
@@ -10,5 +10,143 @@ RSpec.configure do |config|
         }
       } '
     )
+
+    stub_request(:post, 'https://api.stripe.com/v1/customers').to_return(
+      headers: { 'Content-Type' => 'application/json' },
+      body: ' {
+        "id": "cus_xxxxxxxxxxxx",
+        "object": "customer",
+        "account_balance": 0,
+        "created": 1485656258,
+        "currency": "usd",
+        "default_source": "card_xxxxxxxxxxxxxxxxxxxxx",
+        "delinquent": false,
+        "description": "5",
+        "discount": null,
+        "email": null,
+        "livemode": false,
+        "metadata": {
+        },
+        "shipping": null,
+        "sources": {
+          "object": "list",
+          "data": [
+            {
+              "id": "card_xxxxxxxxxxxxxxxxxxxx",
+              "object": "card",
+              "address_city": null,
+              "address_country": null,
+              "address_line1": null,
+              "address_line1_check": null,
+              "address_line2": null,
+              "address_state": null,
+              "address_zip": null,
+              "address_zip_check": null,
+              "brand": "Visa",
+              "country": "US",
+              "customer": "cus_xxxxxxxxxxxxx",
+              "cvc_check": "pass",
+              "dynamic_last4": null,
+              "exp_month": 2,
+              "exp_year": 2025,
+              "funding": "credit",
+              "last4": "4242",
+              "metadata": {
+              },
+              "name": null,
+              "tokenization_method": null
+            }
+          ],
+          "has_more": false,
+          "total_count": 1,
+          "url": "/v1/customers/cus_xxxxxxxxxxxx/sources"
+        },
+        "subscriptions": {
+        "object": "list",
+        "data": [],
+        "has_more": false,
+        "total_count": 0,
+        "url": "/v1/customers/cus_xxxxxxxxxxxxx/subscriptions"
+        }
+      } '
+    )
+    stub_request(:post, 'https://api.stripe.com/v1/charges').to_return(
+      headers: { 'Content-Type' => 'application/json' },
+      body: ' {
+        "id": "ch_xxxxxxxxxxxxxxxxxxxxxxxxx",
+        "object": "charge",
+        "amount": 999,
+        "amount_refunded": 0,
+        "application": null,
+        "application_fee": null,
+        "balance_transaction": "txn_xxxxxxxxxxxxxxxxxxxxxxx",
+        "captured": true,
+        "created": 1485657628,
+        "currency": "usd",
+        "customer": "cus_xxxxxxxxxxxxx",
+        "description": null,
+        "destination": null,
+        "dispute": null,
+        "failure_code": null,
+        "failure_message": null,
+        "fraud_details": {
+        },
+        "invoice": "in_xxxxxxxxxxxxxxxxxxxxxxxx",
+        "livemode": false,
+        "metadata": {
+        },
+        "order": null,
+        "outcome": {
+          "network_status": "approved_by_network",
+          "reason": null,
+          "risk_level": "normal",
+          "seller_message": "Payment complete.",
+          "type": "authorized"
+        },
+        "paid": true,
+        "receipt_email": null,
+        "receipt_number": null,
+        "refunded": false,
+        "refunds": {
+          "object": "list",
+          "data": [
+
+          ],
+          "has_more": false,
+          "total_count": 0,
+          "url": "/v1/charges/ch_xxxxxxxxxxxxxxxxxxxxxxxxx/refunds"
+        },
+        "review": null,
+        "shipping": null,
+        "source": {
+          "id": "card_xxxxxxxxxxxxxxxxxxxxxxxxxx",
+          "object": "card",
+          "address_city": null,
+          "address_country": null,
+          "address_line1": null,
+          "address_line1_check": null,
+          "address_line2": null,
+          "address_state": null,
+          "address_zip": null,
+          "address_zip_check": null,
+          "brand": "Visa",
+          "country": "US",
+          "customer": "cus_xxxxxxxxxxxxx",
+          "cvc_check": null,
+          "dynamic_last4": null,
+          "exp_month": 12,
+          "exp_year": 2017,
+          "funding": "credit",
+          "last4": "4242",
+          "metadata": {
+          },
+          "name": null,
+          "tokenization_method": null
+        },
+        "source_transfer": null,
+        "statement_descriptor": null,
+        "status": "succeeded"
+      } '
+    )
   end
 end
