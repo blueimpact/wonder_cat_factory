e4539247a0d1ceae67e0caaaacc3c06285186e2a
Masaki Takehara <morisot64@gmail.com>
2017-04-23 15:44:41 +0900

    Add event_id to comments.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413144254) do
+ActiveRecord::Schema.define(version: 20170423063920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,10 @@ ActiveRecord::Schema.define(version: 20170413144254) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "event_id"
   end
 
+  add_index "comments", ["event_id"], name: "index_comments_on_event_id", using: :btree
   add_index "comments", ["product_id"], name: "index_comments_on_product_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
@@ -151,6 +153,7 @@ ActiveRecord::Schema.define(version: 20170413144254) do
 
   add_foreign_key "bids", "products"
   add_foreign_key "bids", "users"
+  add_foreign_key "comments", "events"
   add_foreign_key "comments", "products"
   add_foreign_key "comments", "users"
   add_foreign_key "events", "bids"
